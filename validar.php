434a35abb00bcf9c1b55e748f470df4966102de3
_Arom <117594980+Ranksine@users.noreply.github.com>
2024-11-11 20:39:53 -0600

    Update_errors
    TryCatch implementación

--- validar.php
+++ validar.php
@@ -5,26 +5,30 @@ $contraseña=$_POST['password'];
 session_start();
 $_SESSION['usuario']=$usuario;
 
+try{
+  $conexion=mysqli_connect("localhost","root","","db_login");
 
-$conexion=mysqli_connect("localhost","root","","db_login");
+  $consulta="SELECT*FROM USERS where USERNAME='$usuario' and PASSWORD='$contraseña'";
+  $resultado=mysqli_query($conexion,$consulta);
 
-$consulta="SELECT*FROM USERS where USERNAME='$usuario' and PASSWORD='$contraseña'";
-$resultado=mysqli_query($conexion,$consulta);
+  $filas=mysqli_num_rows($resultado);
 
-$filas=mysqli_num_rows($resultado);
+  if($filas){
+      header("location:home.php");
+  }else{
+      ?>
+      <?php
+      include("index.html");
 
-if($filas){
-  
-    header("location:home.php");
-
-}else{
     ?>
+    <h1 class="bad">Datos Incorrectos, Verif&iacute;calos Por Favor</h1>
     <?php
-    include("index.html");
-
-  ?>
-  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
-  <?php
+  }
+}catch(Exception $e){
+  echo 'Message: ' .$e->getMessage();
+}finally{
+  echo 'holision';
+  mysqli_free_result($resultado);
+  mysqli_close($conexion);
 }
-mysqli_free_result($resultado);
-mysqli_close($conexion);
+
